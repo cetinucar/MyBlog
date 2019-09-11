628da0858c39bd4e6b564a5c1d0580b746435a86
Çetin Uçar <cetinnucar@hotmail.com>
2019-09-11 14:20:52 +0300

    DAL Çalışma

---
+++ MyBlog.DAL/Contexts/MyBlogContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.DAL.Config;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.Contexts
+{
+    class MyBlogContext : DbContext
+    {
+        public MyBlogContext(DbContextOptions<MyBlogContext> options) : base(options)
+        {
+            //Veritabanının oluşturulduğundan emin ol
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new PostConfiguration());
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+        }
+
+
+        DbSet<Category> Categories { get; set; }
+
+        DbSet<User> Users { get; set; }
+
+        DbSet<Post> Posts { get; set; }
+
+    }
+}
